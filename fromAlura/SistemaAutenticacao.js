08e46403c085dfa381a20f26970afc42ae972059
Thiago Bardini <thiagobardini@hotmail.com>
2020-12-09 11:41:38 -0500

    phase2-4

--- fromAlura/SistemaAutenticacao.js
+++
@@ -1,9 +0,0 @@
-/*
-Ser autenticavel significa ter o método autenticar
-*/
-
-export class SistemaAutenticacao{
-    static login(autenticavel, senha){
-       return autenticavel.autenticar(senha);
-    }
-}
